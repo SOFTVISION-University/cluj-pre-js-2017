70944692acc514bfd7bf7172d4e0d2f8bd6e404d
Gavriil1994 <gavriil.petrov@softvision.ro>
2017-08-28 17:58:33 +0300

    day 9

--- loginview.js
+++ loginview.js
@@ -1,7 +1,7 @@
 const LoginForm = function(option) {
     return `
     <section style="margin-top: auto">
-    <div style="background-image:url(feedback-banner.jpg);width:100%;height:100%; opacity: 0.7"/>
+    <div style="background-image:url(assets/feedback-banner.jpg);width:100%;height:100%; opacity: 0.7"/>
      <hgroup >
    <h1>SV</h1>
  </hgroup>   
@@ -30,14 +30,14 @@ const LoginForm = function(option) {
 }
 const SubmitFeedbackSection = function(option) {
     return `<section  style="display:inline-flex; background-color: white">
-    <img src="new feedback.jpg" width="20%" height="300px" >
+    <img src="assets/new feedback.jpg" width="20%" height="300px" >
     
   
 <article style="width:1200px;margin:0 auto;" >
    <div style="display:inline-block; width:75%; padding-left: 30px; vertical-align: middle">
-     <h3 style="vertical-align: middle; color:black"> Submit new feedback
+     <h3 style="vertical-align: middle; color:black"> Submit new feedback
        </h3>
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
      </p>
    </div>
@@ -54,13 +54,13 @@ const SubmitFeedbackResults = function(option) {
    <article style="width:1200px;margin:0 auto;" >
         
        <div style="display:inline-block; width:75%; padding-right: 30px; vertical-align: middle">
-       <h3 style="color:black"> View feedback results
+       <h3 style="color:black"> View feedback results
 
-</h3> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+</h3> <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
     </p>
        </div>
-       <img src="results.jpg" width="20%" height="250px" style="vertical-align: middle" />
+       <img src="assets/results.jpg" width="20%" height="250px" style="vertical-align: middle" />
     </article>
 
     </section >`;
